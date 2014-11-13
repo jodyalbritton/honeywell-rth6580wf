5ec49d858eddb9714c19db5bc75250ca54b18c66
Jody Albritton <jody@getoptimus.com>
2014-11-12 22:42:54 -0600

    Attribution

--- thermostat.groovy
+++ thermostat.groovy
@@ -1,6 +1,8 @@
 /**
  *  Total Comfort API
  *
+ *  Based on code by Eric Thomas
+ * 
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
